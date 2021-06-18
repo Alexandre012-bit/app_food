a48a7cdcab887b1346e0e25a07a1620ec8d4551e
Alexandre012-bit <alcorp2021@gmail.com>
2021-06-18 17:01:34 -0300

    criando http

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Restaurant} from './restaurant.model'
+import { Component, Input, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant.model'
+
 @Component({
   selector: 'mt-restaurant',
   templateUrl: './restaurant.component.html'
 })
 export class RestaurantComponent implements OnInit {
 
-  @Input () restaurant: Restaurant 
-  
+  @Input() restaurant: Restaurant
+
   constructor() { }
+
 
   ngOnInit() {
   }
